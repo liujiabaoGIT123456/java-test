beb92714e5a831fb7df385c8bdab134986f95f7c
刘佳宝 <834126420@qq.com>
2020-12-24 14:19:30 +0800

    配置文件

--- src/main/java/com/testall/demo/dao/TestDao.java
+++ src/main/java/com/testall/demo/dao/TestDao.java
@@ -2,10 +2,12 @@ package com.testall.demo.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.testall.demo.entity.Test;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface TestDao extends BaseMapper<Test> {
+    //@Select("select * from TEST")
     List<Test> test();
 }
