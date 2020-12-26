a624e8051ca2575c8f3ac8c8cd4a9ffe4015ccd1
刘佳宝 <834126420@qq.com>
2020-12-27 00:39:58 +0800

    label转换

--- src/main/java/com/testall/demo/controller/TestDBController.java
+++ src/main/java/com/testall/demo/controller/TestDBController.java
@@ -43,6 +43,8 @@ public class TestDBController {
     //连接数据库测试
     @RequestMapping("/test")
     public List<Test> test() {
+        List<Test> test = testDBDao.test();
+        System.out.println(test);
         return testDBDao.test();
     }
 
@@ -52,18 +54,19 @@ public class TestDBController {
         return testDBDao.testPlus();
     }
 
-    private static final Algorithm ALGORITHM= Algorithm.HMAC256("d84b297d50f3c726563372e5c3f5c66a");
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256("d84b297d50f3c726563372e5c3f5c66a");
+
     public static String encode() {
         //通过秘钥生成一个算法
         String token = JWT.create()
                 //设置签发者
                 //.withIssuer("test")
                 //设置过期时间为一个小时
-                .withExpiresAt(new Date(System.currentTimeMillis()+3*60*60*1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3 * 60 * 60 * 1000))
                 //设置用户信息
-                .withClaim("name","liqixiu")
-                .withClaim("email","liqixiu@cmschina.com.cn")
-                .withClaim("iat",new Date())
+                .withClaim("name", "liqixiu")
+                .withClaim("email", "liqixiu@cmschina.com.cn")
+                .withClaim("iat", new Date())
                 //.withClaim("return_to","www.baidu.com")
                 .sign(ALGORITHM);
         return token;
@@ -72,18 +75,17 @@ public class TestDBController {
     //测试
 
 
-
     //list分页
     public static void main3(String[] args) {
-        List<Map<Integer,Integer>> list=new ArrayList();
-        for(int i=0;i<10;i++){
-            Map map=new HashMap<>();
-            map.put(i,i);
+        List<Map<Integer, Integer>> list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            Map map = new HashMap<>();
+            map.put(i, i);
             list.add(map);
         }
-        List<Map<Integer,Integer>> collect = list.stream().skip(5*(2-1)).limit(5).collect(Collectors.toList());
-        int i=0;
-        for(Map<Integer,Integer> test:collect){
+        List<Map<Integer, Integer>> collect = list.stream().skip(5 * (2 - 1)).limit(5).collect(Collectors.toList());
+        int i = 0;
+        for (Map<Integer, Integer> test : collect) {
             System.out.println(test.get(i));
             i++;
         }
@@ -94,14 +96,14 @@ public class TestDBController {
     public static void main2(String[] args) {
         //String jsonStr = JSONUtil.parse(brand).toString();
         //PmsBrand brandBean = JSONUtil.toBean(jsonStr, PmsBrand.class);
-        List<Map<String,String>> lis=new ArrayList<>();
-        Map<String,String> map=new HashMap<>();
-        map.put("1","2");
+        List<Map<String, String>> lis = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "2");
         lis.add(map);
         String s = JSONUtil.parse(lis).toString();
         System.out.println(s);
         List<Map> maps = JSONUtil.toList(new JSONArray(s), Map.class);
-        for(Map map1:maps){
+        for (Map map1 : maps) {
             map1.keySet().forEach(System.out::println);
 
         }
@@ -111,13 +113,13 @@ public class TestDBController {
 
     //正则分割和获取数据
     public static void main1(String[] args) {
-        String a="a2019.01 - 2019.01b2c2019.01 - 2019.01d5asdfa2019.01 - asf";
+        String a = "a2019.01 - 2019.01b2c2019.01 - 2019.01d5asdfa2019.01 - asf";
         String[] split = a.split("[0-9]{4}\\.[0-9]{2}\\s-\\s([0-9]{4}\\.[0-9]{2})?");
         Arrays.stream(split).forEach(System.out::println);
 
-        Pattern p=Pattern.compile("[0-9]{4}\\.[0-9]{2}\\s-\\s([0-9]{4}\\.[0-9]{2})?");
-        Matcher m=p.matcher("a2019.01 - 2019.01b2c2019.01 - 2019.01d5asdfa2019.01 - asf");
-        while(m.find()) {
+        Pattern p = Pattern.compile("[0-9]{4}\\.[0-9]{2}\\s-\\s([0-9]{4}\\.[0-9]{2})?");
+        Matcher m = p.matcher("a2019.01 - 2019.01b2c2019.01 - 2019.01d5asdfa2019.01 - asf");
+        while (m.find()) {
             System.out.println(m.group());
         }
 
@@ -205,16 +207,16 @@ public class TestDBController {
 
             List<Map<String, String>> newMaps = new ArrayList<>();
             Map<String, String> map1 = new LinkedHashMap<>();
-            String[] split = {"债券预警项","判断条件","是否达到预警条件","具体说明","详情备注"};
+            String[] split = {"债券预警项", "判断条件", "是否达到预警条件", "具体说明", "详情备注"};
             int i = 0;
             for (String exportRow : split) {
                 writer.addHeaderAlias(exportRow, exportRow).autoSizeColumn(i);
                 i += 1;
-                map1.put(exportRow,i+"");
+                map1.put(exportRow, i + "");
             }
             newMaps.add(map1);
             writer.merge(split.length - 1, "债券违约预警详情展示", true);
-            List<String> a=new ArrayList<>();
+            List<String> a = new ArrayList<>();
             a.add("说明：发债主体为中信证券。本管理人共有xx只产品持有该主题的债券，产品名分别为xxx、xxx…");
             a.add("数据更新日期：20200817");
             writer.writeRow(a);
@@ -231,10 +233,6 @@ public class TestDBController {
             // 关闭writer，释放内存
             writer.close();
             byte[] bytes = byteArrayOutputStream.toByteArray();
-
-
-
-
 
 
             os.write(bytes);
@@ -261,6 +259,7 @@ public class TestDBController {
 
     /**
      * 标签转换
+     *
      * @param file
      * @throws IOException
      */
@@ -270,29 +269,29 @@ public class TestDBController {
         ExcelReader reader = ExcelUtil.getReader(inputStream);
         List<Map<String, Object>> maps = reader.readAll();
 
-        List<Map<String, Object>> values=new ArrayList<>();
-        for(Map<String, Object> map:maps){
+        List<Map<String, Object>> values = new ArrayList<>();
+        for (Map<String, Object> map : maps) {
             List<String> collect = map.keySet().stream().map(a -> a + "").collect(Collectors.toList());
             collect.remove("L_ID");
             List<String> other = collect.stream().filter(a -> a.contains("其他")).collect(Collectors.toList());
             List<String> notOther = collect.stream().filter(a -> !a.contains("其他")).collect(Collectors.toList());
-            for(String item:notOther){
+            for (String item : notOther) {
                 Object item1 = map.get(item);
-                Object item2 = map.get(item+"（其他）");
-                if((item1==null|| StringUtils.isBlank(item1+""))&&(item2==null|| StringUtils.isBlank(item2+""))){
+                Object item2 = map.get(item + "（其他）");
+                if ((item1 == null || StringUtils.isBlank(item1 + "")) && (item2 == null || StringUtils.isBlank(item2 + ""))) {
                     continue;
                 }
-                if(StringUtils.isBlank(item1+"")){
-                    item1=null;
+                if (StringUtils.isBlank(item1 + "")) {
+                    item1 = null;
                 }
-                if(StringUtils.isBlank(item2+"")){
-                    item2=null;
+                if (StringUtils.isBlank(item2 + "")) {
+                    item2 = null;
                 }
                 Map<String, Object> label = new HashMap<>();
-                label.put("L_ID",map.get("L_ID"));
-                label.put("LABEL",item);
-                label.put("LABEL_VALUE",item1==null?"":item1);
-                label.put("OTHER_VALUE",item2==null?"":item2);
+                label.put("L_ID", map.get("L_ID"));
+                label.put("LABEL", item);
+                label.put("LABEL_VALUE", item1 == null ? "" : item1);
+                label.put("OTHER_VALUE", item2 == null ? "" : item2);
                 label.put("LABEL_GROUP", EnumAll.LabelEnum.getType(item));
                 values.add(label);
             }
@@ -302,9 +301,35 @@ public class TestDBController {
         test.setValue(s);
         test.setId(UUID.randomUUID().toString());
         testDBDao.insert(test);
-        List<Map> maps1= JSONUtil.toList(new JSONArray(s), Map.class);
+        List<Map> maps1 = JSONUtil.toList(new JSONArray(s), Map.class);
         System.out.println(values.size());
 
+    }
+
+    @RequestMapping("/downLoadLabel")
+    public void downLoadLabel(HttpServletResponse response) throws UnsupportedEncodingException {
+        response.setContentType("application/octet-stream");
+        response.setHeader("content-type", "application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;fileName=" + URLEncoder.encode("filename.xlsx", "utf8"));
+        byte[] buffer = new byte[1024];
+        //输出流
+        OutputStream os = null;
+        try {
+            List<Test> test = test();
+            String value = test.get(0).getValue();
+            List<Map> maps1 = JSONUtil.toList(new JSONArray(value), Map.class);
+            os = response.getOutputStream();
+            ExcelWriter writer = ExcelUtil.getWriter();
+            writer.write(maps1);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
+            writer.flush(byteArrayOutputStream);
+            // 关闭writer，释放内存
+            writer.close();
+            byte[] bytes = byteArrayOutputStream.toByteArray();
+            os.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
